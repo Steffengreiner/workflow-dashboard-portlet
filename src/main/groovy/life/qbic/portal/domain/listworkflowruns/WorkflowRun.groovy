c58260474c29af23e4f32826f95e8dd2ce93998c
Steffengreiner <Steffen.Greiner@gmx.de>
2020-01-10 17:28:07 +0100

    Added Grid to TabSheet as Basis for detailed View Dummy Data gets correctly pulled.

--- src/main/groovy/life/qbic/portal/domain/listworkflowruns/WorkflowRun.groovy
+++ src/main/groovy/life/qbic/portal/domain/listworkflowruns/WorkflowRun.groovy
@@ -1,12 +1,96 @@
 package life.qbic.portal.domain.listworkflowruns
 
+// Define WorkflowRun Class containing necessary attributes for the user
+
 class WorkflowRun {
-    String runName
-    String runningTaskName
-    Date startTime
-    def currentRunStatus
-    int submittedCount
-    int runningCount
-    int completedCount
-    int failedCount
+
+    WorkflowRun()
+    {
+    }
+
+    WorkflowRun(String runName, String runningTaskName, Date startTime, WorkflowRunStatus currentRunStatus, int submittedCount, int runningCount, int completedCount, int failedCount) {
+
+        this.runName = runName
+        this.runningTaskName = runningTaskName
+        this.startTime = startTime
+        this.currentRunStatus = currentRunStatus
+        this.submittedCount = submittedCount
+        this.runningCount = runningCount
+        this.completedCount = completedCount
+        this.failedCount = failedCount
+    }
+
+    String runName;
+    String runningTaskName;
+    Date startTime;
+    WorkflowRunStatus currentRunStatus;
+    int submittedCount;
+    int runningCount;
+    int completedCount;
+    int failedCount;
+
+    String getRunName() {
+        return runName
+    }
+
+    void setRunName(String runName) {
+        this.runName = runName
+    }
+
+    String getRunningTaskName() {
+        return runningTaskName
+    }
+
+    void setRunningTaskName(String runningTaskName) {
+        this.runningTaskName = runningTaskName
+    }
+
+    Date getStartTime() {
+        return startTime
+    }
+
+    void setStartTime(Date startTime) {
+        this.startTime = startTime
+    }
+
+    WorkflowRunStatus getCurrentRunStatus() {
+        return currentRunStatus
+    }
+
+    void setCurrentRunStatus(WorkflowRunStatus currentRunStatus) {
+        this.currentRunStatus = currentRunStatus
+    }
+
+    int getSubmittedCount() {
+        return submittedCount
+    }
+
+    void setSubmittedCount(int submittedCount) {
+        this.submittedCount = submittedCount
+    }
+
+    int getRunningCount() {
+        return runningCount
+    }
+
+    void setRunningCount(int runningCount) {
+        this.runningCount = runningCount
+    }
+
+    int getCompletedCount() {
+        return completedCount
+    }
+
+    void setCompletedCount(int completedCount) {
+        this.completedCount = completedCount
+    }
+
+    int getFailedCount() {
+        return failedCount
+    }
+
+    void setFailedCount(int failedCount) {
+        this.failedCount = failedCount
+    }
+
 }
--- src/main/groovy/life/qbic/portal/infrastructure/view/PortletMainView.groovy
+++ src/main/groovy/life/qbic/portal/infrastructure/view/PortletMainView.groovy
@@ -1,17 +1,81 @@
 package life.qbic.portal.infrastructure.view
 
+import com.vaadin.ui.Grid
 import com.vaadin.ui.Layout
+import com.vaadin.ui.ListSelect
+import com.vaadin.ui.TabSheet
+import com.vaadin.ui.TextField
 import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.HorizontalLayout
+import life.qbic.portal.infrastructure.services.workflowtracking.Dummydata
 import life.qbic.portal.infrastructure.view.workflowruninfolist.WorkflowRunInfoListViewModelObserver
+import life.qbic.portal.infrastructure.view.workflowruninfolist.WorkflowRunListGrid
+import life.qbic.portal.infrastructure.view.workflowruninfolist.WorkflowRunTab
+
 
 class PortletMainView extends VerticalLayout implements WorkflowRunInfoListViewModelObserver, Layout {
 
+
     PortletMainView() {
+
         super()
         createContent()
-    }
-
-    private def createContent() {
 
     }
+
+    /*Current Status is to Just Run all Commands in Portlet Main View for Testing,
+    this will change in further development */
+
+    private createContent() {
+
+        //Dummy Components to Test Layout
+
+        TextField labelField = new TextField()
+        labelField.setValue("Value")
+
+        TextField labelField2 = new TextField()
+        labelField.setValue("Value2")
+
+        // "Read in" (Dummy) Data from Database
+
+        Dummydata Dummydatamaker = new Dummydata()
+        List dummydata = Dummydatamaker.datagen()
+
+        // List to test if dummy data was imported
+
+        List namelist = []
+        for (item in dummydata){
+            namelist.add(item.getRunName())
+        }
+
+        ListSelect<String> select = new ListSelect<>("The List")
+        select.setItems(namelist)
+        select.setRows(5)
+        this.addComponent(select)
+
+        VerticalLayout tablayout1 = new VerticalLayout()
+        WorkflowRunTab tabsheetmaker = new WorkflowRunTab()
+        HorizontalLayout tablayout2 = new HorizontalLayout();
+
+        WorkflowRunListGrid Gridmaker = new WorkflowRunListGrid()
+        Grid grid = Gridmaker.createGrid(dummydata)
+
+        // Add Grid to Layout
+        //this.addComponent(grid)
+
+        TabSheet tabsheet = tabsheetmaker.generatetabs(tablayout2, grid, "WorkflowBoard")
+        tabsheetmaker.generatetabs(tablayout1, labelField, "Details")
+
+        this.addComponent(tabsheet)
+
+
+
+
+
+    }
+
 }
+
+
+
+
